d716e5ff4e339cc66ab52ab804209d807c0c1dbf
christopherross <christopherross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-13 23:15:49 +0000

    git-svn-id: https://plugins.svn.wordpress.org/tynt-insight-for-wordpress/trunk@252250 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ TyntInsightforWordPress.php
@@ -0,0 +1,150 @@
+<?php
+/*
+Plugin Name: Tynt Insight for WordPress
+Plugin URI: http://regentware.com/software/web-based/wordpress-plugins/tynt-insight-plugin-for-wordpress/
+Description: Learn what's being copied off your website and how you can leverage this behaviour to get more traffic, more often.
+Author: Christopher Ross
+Author URI: http://thisismyurl.com
+Version: 0.0.1
+*/
+
+/*  Copyright 2008  Christopher Ross  (email : info@thisismyurl.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+add_action('wp_footer', 'cr_tynt_insight_footer_code');
+
+
+add_action('admin_menu', 'cr_tynt_insight_menu');
+add_filter('plugin_action_links', 'cr_tynt_insight_plugin_actions', 10, 2);
+
+register_activation_hook(__FILE__, 'cr_tynt_insight_activate');
+
+function cr_tynt_insight_menu() {
+  add_options_page('Tynt Insight for WordPress', 'Tynt Insight for WordPress', 10,'cr_tynt_insight.php', 'cr_tynt_insight_options');
+  
+}
+
+function cr_tynt_insight_activate() {
+	if (!(get_option('cr_tynt_insight_give_credit')))	 	{update_option('cr_tynt_insight_give_credit','1');}
+}
+
+function cr_tynt_insight_footer_code($options='') {
+
+		echo "<script type='text/javascript'>tyntVariables = {'ap':'Read more: '};</script> <script type='text/javascript' src='http://tcr.tynt.com/javascripts/Tracer.js?user=".get_option('cr_tynt_user_code')."&amp;st=1'></script>";
+	
+	global $cr_credit;
+	if(get_option('cr_tynt_insight_give_credit') == "1" && !$cr_credit) { 
+		echo "<!-- \n\n\n Tynt Insight Plugin for WordPress by Christopher Ross\nhttp://www.thisismyurl.com \n\n\n -->";
+		$cr_credit == true;		
+	}
+	if ((get_option('cr_tynt_insight_check')+(350000)) < date('U')) {cr_tynt_insight_plugin_getupdate();}
+}
+
+function cr_tynt_insight_plugin_actions($links, $file){
+	static $this_plugin;
+
+	if( !$this_plugin ) $this_plugin = plugin_basename(__FILE__);
+
+	if( $file == $this_plugin ){
+		$settings_link = '<a href="options-general.php?page=cr_tynt_insight.php">' . __('Settings') . '</a>';
+		$links = array_merge( array($settings_link), $links); // before other links
+	}
+	return $links;
+}
+
+function cr_tynt_insight_plugin_getupdate() {
+	//update check - to finish
+	update_option('cr_tynt_insight_check',date('U'));
+	
+	$host='downloads.wordpress.org';
+	$ua='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.6) Gecko/20060728 Firefox/1.5';
+
+	$fp = fsockopen($host, 80, $errno, $errstr, 30);
+	if ($fp) {
+	   $out = "GET /plugin/wordpresscom-stats-smiley-remover.zip HTTP/1.1\r\n";
+	   //echo "<pre>$out</pre>\n"; //debug only
+	   $out .= "User-Agent: $ua\r\n";
+	   $out .= "Host: $host\r\n";
+	   $out .= "Connection: Close\r\n\r\n";
+   
+	   fwrite($fp, $out);
+	   
+	   while (!feof($fp)) {
+			$data = fgets($fp, 128);
+			$pos = strpos($data, "Rank_");
+			if($pos === false){} else{
+				$pr=substr($data, $pos + 9);
+				$pr=trim($pr);
+				$pr=str_replace("\n",'',$pr);
+				return $pr;
+			}
+	   }
+	   fclose($fp);
+	}
+}
+
+
+
+function cr_tynt_insight_options($options='') {
+?>
+    
+    <div class="wrap">
+    <h2>Tynt Insight Plugin for WordPress</h2>
+    <form method="post" action="options.php">
+    <?php wp_nonce_field('update-options'); ?>
+    
+    
+    <h3>Plugin Settings</h3>
+    <table class="form-table">
+    
+        <tr valign="top">
+        <th scope="row">Tynt Usercode</th>
+        <td>
+        <input name="cr_tynt_user_code" type="text" id="cr_tynt_user_code" value="<?php echo get_option('cr_tynt_user_code'); ?>" />
+        <?php if(strlen(get_option('cr_tynt_user_code')) > 5) {?>
+		<p><strong><a href='http://id.tynt.com/reports/overview'>View your Tynt stats online</a></strong> to see who's copied your content.</p>
+		 <?php } else { ?>
+        <p>Before Tynt Insight Plugin for WordPress can work, you'll have to sign up for an account at <a href='http://www.tynt.com/'>http://www.tynt.com/</a> and copy your user code found at <a href='http://id.tynt.com/script/account_script'>http://id.tynt.com/script/account_script</a> into the box above.</p>
+        <?php } ?> </td>
+        </tr>
+
+
+        <tr valign="top">
+        <th scope="row">Give Credit</th>
+        <td>
+        <input name="cr_tynt_insight_give_credit" type="checkbox" id="cr_tynt_insight_give_credit" value="1" <?php if(get_option('cr_tynt_insight_give_credit') == "1") {echo  "checked='checked'";} ?> />
+        <p>If you'd like to thank me for creating this plugin, please allow my plugin to place a link to my blog in the footer of your website. The link will appear in the HTML code but not on your website. </p>
+        </td>
+        </tr>
+    
+    
+    </table>
+    
+    <input type="hidden" name="action" value="update" />
+    <input type="hidden" name="page_options" value="cr_tynt_user_code,cr_tynt_insight_give_credit" />
+    
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+    </form>
+    </div>
+
+<?php
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Plugin Name ===
+Contributors: christopherross
+Plugin URI: http://regentware.com/software/web-based/wordpress-plugins/tynt-insight-plugin-for-wordpress/
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5731867
+Tags: copy paste, clipboard, tynt, api, copyright, content theft
+Requires at least: 2.0.0
+Tested up to: 3.0.0
+Stable tag: 0.0.1
+
+Learn what's being copied off your website and how you can leverage this behaviour to get more traffic, more often.
+
+== Description ==
+
+How would you like to know who's copying content from your website and turn those users into return visitors?
+
+We found a wonderful website called Tynt a few weeks back and we fell in love with the tool, the reports and the functionality but found it a little difficult to impliment for the average user so we built the Tynt Insight Plugin for WordPress, a free downloadable tool that allows you to easily add Tynt's amazing copy & paste functionality onto any website.
+
+Want to see it in action? Simply copy from this website and paste it into a new document, presto, a link appears back to the source page you copied from!
+
+With the Tynt Insight Plugin for WordPress, you can easily add Tynt to any WordPress blog or website as quickly and easily as other popular API platforms such as WP Stats or Askimet, just copy and paste the API data into your Tynt Insight Plugin for WordPress control panel and you're ready to go!
+
+== Installation ==
+
+To install the plugin, please upload the folder to your plugins folder and active the plugin.
+
+== Updates ==
+Updates to the plugin will be posted here, to [thisismyurl.com](http://thisismyurl.com/)
+
+== Frequently Asked Questions ==
+
+== Donations ==
+If you would like to donate to help support future development of this tool, please visit [thisismyurl](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5731867)
+
+== Change Log ==
